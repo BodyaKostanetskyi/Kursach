2ec48dff266582decdc648cb5bbbd6f96530ebf9
Богдан <90748478+BodyaKostanetskyi@users.noreply.github.com>
2023-06-12 12:40:38 +0300

    Курсач

---
+++ MultyTestApp/MultyTestApp/Data/Survey.cs
@@ -0,0 +1,18 @@
+﻿namespace MultyTestApp.Data
+{
+	public class Survey
+	{
+        public string Question { get; set; }
+        public string Answer { get; set; }
+		public Survey(string Question, string Answer)
+		{
+			this.Question = Question;
+			this.Answer = Answer;
+		}
+
+		public bool CheckAnswer(string Answer)
+		{
+			return this.Answer.Equals(Answer);
+		}
+	}
+}
